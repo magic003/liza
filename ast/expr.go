c7f345df544b986dc20bc102c470d210a4a4fdaf
Minjie Zha <minjiezha@gmail.com>
2017-11-19 13:18:49 -0800

    add Ident expression

--- ast/expr.go
+++ ast/expr.go
@@ -1,7 +1,33 @@
 package ast
 
+import (
+	"github.com/magic003/liza/token"
+)
+
 // Expr is the base type for all expression tree node.
 type Expr interface {
 	Node
 	exprNode()
 }
+
+// Ident is a node represents an identifier.
+type Ident struct {
+	NamePos token.Position // identifier position
+	Name    string         // identifier name
+}
+
+// Pos implementation for Node.
+func (ident *Ident) Pos() token.Position {
+	return ident.NamePos
+}
+
+// End implementation for Node.
+func (ident *Ident) End() token.Position {
+	return token.Position{
+		Filename: ident.NamePos.Filename,
+		Line:     ident.NamePos.Line,
+		Column:   ident.NamePos.Column + len(ident.Name),
+	}
+}
+
+func (ident *Ident) exprNode() {}
---
+++ ast/expr_test.go
@@ -0,0 +1,61 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/magic003/liza/token"
+)
+
+var testCases = []struct {
+	desc        string
+	expr        Expr
+	expectedPos token.Position
+	expectedEnd token.Position
+}{
+	{
+		desc: "Ident",
+		expr: &Ident{
+			NamePos: token.Position{
+				Filename: "test.lz",
+				Line:     10,
+				Column:   26,
+			},
+			Name: "testVar",
+		},
+		expectedPos: token.Position{
+			Filename: "test.lz",
+			Line:     10,
+			Column:   26,
+		},
+		expectedEnd: token.Position{
+			Filename: "test.lz",
+			Line:     10,
+			Column:   26 + 7,
+		},
+	},
+}
+
+func TestExpr(t *testing.T) {
+	for _, tc := range testCases {
+		results := []token.Position{tc.expr.Pos(), tc.expr.End()}
+		expectedResults := []token.Position{tc.expectedPos, tc.expectedEnd}
+		names := []string{"Pos()", "End()"}
+
+		for i, result := range results {
+			if result.Filename != expectedResults[i].Filename {
+				t.Errorf("bad filename for %s %s: got %s, expected %s",
+					tc.desc, names[i], result.Filename, expectedResults[i].Filename)
+			}
+
+			if result.Line != expectedResults[i].Line {
+				t.Errorf("bad line for %s %s: got %s, expected %s",
+					tc.desc, names[i], result.Line, expectedResults[i].Line)
+			}
+
+			if result.Column != expectedResults[i].Column {
+				t.Errorf("bad column for %s %s: got %s, expected %s",
+					tc.desc, names[i], result.Column, expectedResults[i].Column)
+			}
+		}
+	}
+}
