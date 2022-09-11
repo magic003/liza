e22196341ddc88c094735c4a7c866c2565275f99
Minjie Zha <minjiezha@gmail.com>
2022-09-10 18:39:04 -0700

    add comments

--- liza/token/pos.v
+++ liza/token/pos.v
@@ -1,11 +1,13 @@
 module token
 
+// Pos is the position in a source file.
 pub struct Pos {
 	filename string
 	line     int
 	column   int
 }
 
+// str returns the Pos in a human readable text.
 pub fn (p Pos) str() string {
 	return '$p.filename:$p.line,$p.column'
 }
